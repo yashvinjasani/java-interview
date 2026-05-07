67806ff7bbe6d35db941695085e7e47f0d59d473
Yashvin Jasani <163496031+yashvinjasani@users.noreply.github.com>
2026-05-06 22:29:27 -0400

    Create Desginer PDF.java

---
+++ Problems/Desginer PDF.java
@@ -0,0 +1,62 @@
+//Desginer PDF viewer
+//Problem Description:
+//https://www.hackerrank.com/challenges/designer-pdf-viewer/problem?isFullScreen=true
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     * 1. INTEGER_ARRAY h
+     * 2. STRING word
+     */
+
+    public static int designerPdfViewer(List<Integer> h, String word) {
+        int maxHeight = 0;
+        //only get heights of charaters in word so loop over w o r d 
+        //check the height of character in given list h, corresponding index
+        for (int i = 0; i < word.length(); i++) {
+            int height = h.get(word.charAt(i) - 'a');
+            if (height > maxHeight) {
+                maxHeight = height;
+            }
+        }
+        return maxHeight * word.length();
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> h = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        String word = bufferedReader.readLine();
+
+        int result = Result.designerPdfViewer(h, word);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
