05fce4b0c7906c3c1ff82c80c749df7e05554613
Yashvin Jasani <163496031+yashvinjasani@users.noreply.github.com>
2026-05-08 23:52:51 -0400

    Create Beautiful Days at Movies.java

---
+++ Problems/Beautiful Days at Movies.java
@@ -0,0 +1,72 @@
+//Problem Description
+//https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem?isFullScreen=true
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'beautifulDays' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER i
+     *  2. INTEGER j
+     *  3. INTEGER k
+     */
+
+    public static int beautifulDays(int i, int j, int k) {
+    int count = 0;
+
+    for (int day = i; day <= j; day++) {
+        int reversed = 0;
+        int temp = day;
+
+        // Mathematically reverse the number (works for any number of digits)
+        while (temp > 0) {
+            reversed = reversed * 10 + (temp % 10);
+            temp /= 10;
+        }
+
+        // Check if the day is "beautiful"
+        if (Math.abs(day - reversed) % k == 0) {
+            count++;
+        }
+    }
+
+    return count;
+  }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int i = Integer.parseInt(firstMultipleInput[0]);
+
+        int j = Integer.parseInt(firstMultipleInput[1]);
+
+        int k = Integer.parseInt(firstMultipleInput[2]);
+
+        int result = Result.beautifulDays(i, j, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
