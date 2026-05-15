da0c432e53b0bbbefc61261c739319a80a4d2cc4
Yashvin Jasani <163496031+yashvinjasani@users.noreply.github.com>
2026-05-15 18:06:34 -0400

    Add extraLongFactorials implementation
    Implement extraLongFactorials function to compute factorials for large integers using BigInteger.

---
+++ Problems/ExtraLongFactorials.java
@@ -0,0 +1,43 @@
+//https://www.hackerrank.com/challenges/extra-long-factorials/problem?isFullScreen=true
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'extraLongFactorials' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void extraLongFactorials(int n) {
+    BigInteger result = new BigInteger(String.valueOf(1));
+    
+    for (int i = 1; i <= n; i++) {
+        result = result.multiply(BigInteger.valueOf(i));
+    }
+    
+    System.out.println(result);
+}
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.extraLongFactorials(n);
+
+        bufferedReader.close();
+    }
+}
