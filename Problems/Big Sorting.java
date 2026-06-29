fe1041f8b39b8d2686631e9a8560b3b72dcf95a5
Yashvin Jasani <163496031+yashvinjasani@users.noreply.github.com>
2026-06-28 21:34:56 -0400

    Add Big Sorting implementation in Java
    Implement bigSorting function to sort large numbers represented as strings.

---
+++ Problems/Big Sorting.java
@@ -0,0 +1,62 @@
+//https://www.hackerrank.com/challenges/big-sorting/problem?isFullScreen=true
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'bigSorting' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts STRING_ARRAY unsorted as parameter.
+     */
+
+    public static List<String> bigSorting(List<String> unsorted) {
+    // Write your code here
+        return unsorted.stream().sorted((s1,s2)->{
+            if(s1.length()!=s2.length()){
+                return Integer.compare(s1.length(), s2.length());
+            }
+            return s1.compareTo(s2);
+        }).collect(Collectors.toList());
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<String> unsorted = IntStream.range(0, n).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .collect(toList());
+
+        List<String> result = Result.bigSorting(unsorted);
+
+        bufferedWriter.write(
+            result.stream()
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
