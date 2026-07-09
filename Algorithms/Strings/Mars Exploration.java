1d52ccf11b0b0070a6a9778210f48a06cf14c3c2
Yashvin Jasani <163496031+yashvinjasani@users.noreply.github.com>
2026-07-08 23:38:23 -0400

    Add Mars Exploration problem solution in Java

---
+++ Algorithms/Strings/Mars Exploration.java
@@ -0,0 +1,54 @@
+//https://www.hackerrank.com/challenges/mars-exploration/problem?isFullScreen=true
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'marsExploration' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int marsExploration(String s) {
+        int alteredCount = 0;
+    String expected = "SOS";
+    
+    for (int i = 0; i < s.length(); i++) {
+        // expected.charAt(i % 3) will continuously loop through 'S', 'O', 'S'
+        if (s.charAt(i) != expected.charAt(i % 3)) {
+            alteredCount++;
+        }
+    }
+    
+    return alteredCount;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.marsExploration(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
