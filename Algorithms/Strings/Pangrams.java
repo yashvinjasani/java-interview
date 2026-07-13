30a60fe4cf1f663459926248bcd2b643319d6c71
Yashvin Jasani <163496031+yashvinjasani@users.noreply.github.com>
2026-07-13 00:01:04 -0400

    Add pangrams function to check string completeness
    Implement pangrams function to check if a string contains all letters of the alphabet.

--- Algorithms/Strings/Pangrams.java
+++ Algorithms/Strings/Pangrams.java
@@ -1,1 +1,50 @@
 //https://www.hackerrank.com/challenges/pangrams/problem?isFullScreen=true
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String pangrams(String s) {
+        long uniqueLetters = s.toLowerCase()
+                          .chars() // Get IntStream of characters
+                          .filter(c -> c >= 'a' && c <= 'z') // Keep only letters
+                          .distinct() // Keep only unique characters
+                          .count(); // Count them
+                          
+    return uniqueLetters == 26 ? "pangram" : "not pangram";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.pangrams(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
