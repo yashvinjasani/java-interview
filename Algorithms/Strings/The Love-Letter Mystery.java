3d2ef6e0a6fe4485ea47519ce98bd416cfddd2a2
Yashvin Jasani <163496031+yashvinjasani@users.noreply.github.com>
2026-08-03 23:01:08 -0400

    Add function to solve Love Letter Mystery problem
    Implement theLoveLetterMystery function to calculate operations needed.

--- Algorithms/Strings/The Love-Letter Mystery.java
+++ Algorithms/Strings/The Love-Letter Mystery.java
@@ -1,1 +1,17 @@
 //https://www.hackerrank.com/challenges/the-love-letter-mystery/problem?isFullScreen=true
+public static int theLoveLetterMystery(String s) {
+    int operations = 0;
+    int n = s.length();
+    
+    // Loop through the first half of the string
+    for (int i = 0; i < n / 2; i++) {
+        // Find the symmetric characters
+        char leftChar = s.charAt(i);
+        char rightChar = s.charAt(n - 1 - i);
+        
+        // Add the absolute difference of their ASCII values to the total
+        operations += Math.abs(leftChar - rightChar);
+    }
+    
+    return operations;
+}
